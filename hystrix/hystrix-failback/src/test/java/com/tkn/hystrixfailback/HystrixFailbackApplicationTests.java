ebee2d6dd0239a62bfd4696b2cb77ffcec8d4970
lizhu <lzlj21@163.com>
2021-07-13 10:20:24 +0800

    guava 工具集合使用

---
+++ hystrix/hystrix-failback/src/test/java/com/tkn/hystrixfailback/HystrixFailbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkn.hystrixfailback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixFailbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
