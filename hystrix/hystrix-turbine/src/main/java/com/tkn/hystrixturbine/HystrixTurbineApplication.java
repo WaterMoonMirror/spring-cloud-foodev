582d5ebe5a5d60066ee187d9fba4ec269339ea8c
lizhu <lzlj21@163.com>
2021-07-14 10:13:12 +0800

    hystrix-tubine

---
+++ hystrix/hystrix-turbine/src/main/java/com/tkn/hystrixturbine/HystrixTurbineApplication.java
@@ -0,0 +1,18 @@
+package com.tkn.hystrixturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableHystrix
+@EnableTurbine
+public class HystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixTurbineApplication.class, args);
+    }
+
+}
