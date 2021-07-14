703afc3d13677f398a48ee55ddeb995cddb5960a
lizhu <lzlj21@163.com>
2021-07-14 10:12:57 +0800

    hystrix-tubine

---
+++ 996/guaval-demo/src/test/java/com/tkn/guavaldemo/list/Stu.java
@@ -0,0 +1,13 @@
+package com.tkn.guavaldemo.list;
+
+import java.io.Serializable;
+
+/**
+ * @author: lizhu@wondesgroup.com
+ * @date: 2021/7/13 10:34
+ * @description:
+ */
+public class Stu  implements Serializable {
+
+    private static final long serialVersionUID = -6685973102558994978L;
+}
